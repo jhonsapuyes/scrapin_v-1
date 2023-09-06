70d06f96ae7499e403d50a7e7a51b8b939e6edf8
jhonsapuyes <114039549+jhonsapuyes@users.noreply.github.com>
2023-09-06 11:07:35 -0500

    codificando
    abrir en amz

---
+++ index.js
@@ -0,0 +1,41 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+
+
+async function raspado_web(tabla,busqueda,n_paginas){
+  let crear_tabla= tabla
+  let busqueda_articulo= busqueda
+  let numero_paginas_escrapeadas= n_paginas
+  
+    const fecha = new Date();
+    const añoActual = fecha.getFullYear().toString();
+    const hoy = fecha.getDate().toString();
+    const mesActual = (fecha.getMonth()+1).toString();
+    let fecha_actual= añoActual+"-"+mesActual+"-"+hoy
+
+    async function amazon_raspado(fecha,web,pt_1,search,raspar) {
+        const browser = await puppeteer.launch({
+          headless: false,
+          slowMo: 200,
+        });
+        const page = await browser.newPage();
+        await page.goto(pt_1);
+
+        let data_total="hola"
+    
+        await new Promise((resolve) => setTimeout(resolve, 1000)); 
+        await browser.close();
+
+        if (data_total != "") {
+          return data_total;
+        }
+    }
+    let raspado_amazon= await amazon_raspado(fecha_actual, crear_tabla, "https://www.amazon.com/", busqueda_articulo, numero_paginas_escrapeadas);
+
+
+
+    return "datos recoletados"
+}
+
+module.exports= raspado_web;
